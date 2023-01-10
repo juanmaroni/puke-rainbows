1efb705cdfe207016877763bc2324bf38faa9653
Juanma Romero <pro@juanmaroni.me>
2023-01-10 11:24:12 +0100

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 use std::f64::consts::PI;
 
+// Print text with RGB colors
+pub fn print_colored(text: String) {
+    text.chars()
+        .enumerate()
+        .for_each(|(i, c)| {
+            let (r, g, b) = get_rgb_color(i);
+            print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c); // ANSI
+        });
+    print!("\n");
+}
+
 // Get a color using trigonometry
 fn get_rgb_color(i: usize) -> (u8, u8, u8) {
     let factor = 0.1;
@@ -10,15 +21,4 @@ fn get_rgb_color(i: usize) -> (u8, u8, u8) {
     (((factor * i).sin() * 127.0 + 128.0) as u8,
     ((factor * i + 2.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8,
     ((factor * i + 4.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8)
-}
-
-// Print text with RGB colors
-pub fn print_colored(text: String) {
-    text.chars()
-        .enumerate()
-        .for_each(|(i, c)| {
-            let (r, g, b) = get_rgb_color(i);
-            print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c);
-        });
-    print!("\n");
 }
