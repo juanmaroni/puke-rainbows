b766a55f1d1f5db37bf1fe9a43155b6d4813ae24
Juanma Romero <pro@juanmaroni.me>
2023-01-10 11:04:21 +0100

    Function to print colored text

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,14 @@ fn get_rgb_color(i: usize) -> (u8, u8, u8) {
     ((factor * i + 2.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8,
     ((factor * i + 4.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8)
 }
+
+// Print text with RGB colors
+pub fn print_colored(text: String) {
+    text.chars()
+        .enumerate()
+        .for_each(|(i, c)| {
+            let (r, g, b) = get_rgb_color(i);
+            print!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, c);
+        });
+    print!("\n");
+}
