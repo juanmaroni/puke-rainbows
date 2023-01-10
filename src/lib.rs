536aa4f825168362a1a1cea3b170a0519544feb5
Juanma Romero <pro@juanmaroni.me>
2023-01-10 11:03:41 +0100

    Little fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use std::f64::consts::PI;
 
 // Get a color using trigonometry
-pub fn get_rgb_color(i: u32) -> (u8, u8, u8) {
+fn get_rgb_color(i: usize) -> (u8, u8, u8) {
     let factor = 0.1;
     let i = i as f64;
 
     // These angles are chosen because they are 120 degrees apart on
     // the trigonometric circle, which produces complementary colors.
-    (((factor * i) * 127.0 + 128.0).sin() as u8,
+    (((factor * i).sin() * 127.0 + 128.0) as u8,
     ((factor * i + 2.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8,
     ((factor * i + 4.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8)
 }
