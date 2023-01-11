7bd79f18c319b79730f85aa13a746a807790092e
Juanma Romero <pro@juanmaroni.me>
2023-01-11 08:25:09 +0100

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,18 @@ pub fn print_colored(text: String) {
 
 // Get a color using trigonometry
 fn get_rgb_color(i: usize) -> (u8, u8, u8) {
-    let factor = 0.1;
     let i = i as f64;
 
     // These angles are chosen because they are 120 degrees apart on
     // the trigonometric circle, which produces complementary colors.
-    (((factor * i).sin() * 127.0 + 128.0) as u8,
-    ((factor * i + 2.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8,
-    ((factor * i + 4.0 * PI / 3.0).sin() * 127.0 + 128.0) as u8)
+    (calc_color_channel(i, 0.0) as u8,
+    calc_color_channel(i, 2.0) as u8,
+    calc_color_channel(i, 4.0) as u8)
+}
+
+fn calc_color_channel(i: f64, angle: f64) -> f64 {
+    // Change this and see what happens
+    let factor = 0.1;
+
+    (factor * i + angle * PI / 3.0).sin() * 127.0 + 128.0
 }
