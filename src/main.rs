7eb7c92f20268d5cb6a317012b2988cf50b775bb
Juanma Romero <pro@juanmaroni.me>
2023-01-15 20:19:41 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -55,20 +55,17 @@ fn main() -> Result<()> {
             bail!("Wrong argument!\n\n{}\n", HELP_TEXT);
         }
 
-        if args[0] == "-file" {
-            // Read and print file
-            let output = &get_file_content(&args[1])?;
-            print_colored(&output);
-            generate_ansi_file(&convert_to_colored(&output))?;
+        let output = if args[0] == "-file" {
+            get_file_content(&args[1])?
         } else if args[0] == "-text" {
-            // Print text
-            let output = &args[1];
-            print_colored(&output);
-            generate_ansi_file(&convert_to_colored(&output))?;
+            args[1].to_owned()
         } else {
             // Error
             bail!("Wrong argument!\n\n{}\n", HELP_TEXT);
-        }
+        };
+
+        print_colored(&output);
+        generate_ansi_file(&convert_to_colored(&output))?;
     } else {
         // Error
         bail!("Wrong number of arguments!\n\n{}\n", HELP_TEXT);
