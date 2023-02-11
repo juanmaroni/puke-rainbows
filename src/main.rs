c87e69c73f832b1ffdf9d8bf7751d263153d0520
Juanma Romero <pro@juanmaroni.me>
2023-02-11 21:19:53 +0100

    More info on usage

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ Options:
 \t-help \tShow this screen.
 \t-text \tPrint a given text.
 \t-file \tPrint a given file.
-\t-save \tSave ANSI output as a file named \"puke.txt\".
+\t-save \tSave ANSI output as a file named \"puke.txt\" in the current directory.
 
 Examples:
 \tpuke-rainbows -help
